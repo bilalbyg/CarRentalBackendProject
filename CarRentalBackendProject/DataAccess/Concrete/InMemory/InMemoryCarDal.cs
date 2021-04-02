b2e721aa1f34fb490d0053b46ef03cee9e2e6190
Bilal <bbeygo.71@gmail.com>
2021-04-02 03:16:37 +0300

    initial commit

---
+++ CarRentalBackendProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _carDal;
+        public InMemoryCarDal()
+        {
+            _carDal = new List<Car>();
+        }
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
