198ce291d9c67733fbc722c20ee1be2fbd9fe19e
Bilal <bbeygo.71@gmail.com>
2021-04-12 12:09:29 +0300

    code refactored

--- CarRentalBackendProject/Business/Constants/Messages.cs
+++ CarRentalBackendProject/Business/Constants/Messages.cs
@@ -9,5 +9,8 @@ namespace Business.Constants
         public static string CarAdded = "Car added";
         public static string CarNameInvalid = "Car name invalid";
         public static string MaintenanceTime = "Maintenance time";
+        public static string UserAdded = "User added";
+        public static string UsersListed = "Users listed";
+        public static string UserUpdated = "User updated";
     }
 }
